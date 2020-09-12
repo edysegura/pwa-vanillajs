ebc34b9f748a25c45daef5aac0ddee970ca84669
Edy Segura <edysegura@gmail.com>
2020-09-11 23:05:31 -0300

    Cache Assets

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
+const cacheName = 'no-connection-v1';
+
+const assetsToCache = [
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  'offline.html'
+];
+
+async function cacheStaticAssets() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(assetsToCache);
+}
+
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing service worker...', event);
+  event.waitUntil(cacheStaticAssets());
   self.skipWaiting();
 });
 
