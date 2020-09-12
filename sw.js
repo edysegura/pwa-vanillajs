39314507565c2e41ec0152d4e93a736f05d76b81
Edy Segura <edysegura@gmail.com>
2020-09-11 23:00:25 -0300

    Service Worker Clients Claim

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing service worker...', event);
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   console.log('[Service Worker] Activating service worker...', event);
+  self.clients.claim();
 });
